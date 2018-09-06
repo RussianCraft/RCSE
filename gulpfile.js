db0ba7f89f0d091bcf4531cd05d91c01b67ab229
LoneSimba <vovamaster99@gmail.com>
2018-09-06 11:43:55 +0300

    Massive Node.js intervention! #1

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+//Sass
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var source_dir = "proj/templates/RCSE/scss/**/";
+var final_dir = "proj/templates/RCSE/";
+
+gulp.task('sass', function(){
+    gulp.src(source_dir + '*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(final_dir))
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch(source_dir + '*.scss', ['sass'])
+})
