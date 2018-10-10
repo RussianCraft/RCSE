656dad9d438c6ff7345ab46ddb664500e686f019
LoneSimba <vovamaster99@gmail.com>
2018-10-10 17:17:51 +0300

    Core development #1.2 Updated config templates, started work on Configurator class

--- proj/core/configurator.php
+++ proj/core/configurator.php
@@ -1,12 +1,22 @@
 <?php
-
+declare(strict_types=1);
+require_once("./logger.php");
 /**
  * class Configurator
  * Parses from and to JSON config files
  */
 class Configurator
 {
+   private $logger = new Logger();
 
+    function __construct()
+    {
 
+    }
+
+    function get_main_config() 
+    {
+       
+    }
 
 }
