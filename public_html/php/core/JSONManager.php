f2c28fa80966d876a49b8cd473240e0e1b506966
Vladimir <vovamaster99@gmail.com>
2018-11-09 21:00:44 +0300

    Core development #1.72
    New functions in JSONManager

--- public_html/php/core/JSONManager.php
+++ public_html/php/core/JSONManager.php
@@ -6,9 +6,13 @@ error_reporting(-1);
 
 define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 define("ERROR_PREFIX", "JSONManager Error: ");
-define("ERROR_INCORRECT_CONFIG_TYPE", "Incorrect config type or not selected, check source code of your installation or send this message (with error) to Issues at GitHub!");
+define("ERROR_INCORRECT_CONFIG_TYPE", "Incorrect config type or not selected, check source code of your installation or send this message (with error) to Issues at GitHub!\n");
 define("ERROR_RECONFIG_REQUIRED", "Site should be reconfigured! Redirecting to AdminPanel in 5 seconds!\n");
-define("ERROR_LOCALE_NOT_FOUND", "Selected locale not found or incorrect, check source code of your installation or send this message (with error) to Issues at GitHub!");
+define("ERROR_LOCALE_NOT_FOUND", "Selected locale not found or incorrect, check source code of your installation or send this message (with error) to Issues at GitHub!\n");
+define("ERROR_CONFIG_STRUCTURE", "Tried to write config, but structure does not match!\n");
+define("ERROR_CONFIG_UPDATE", "Tried to write new config data, but failed! Check your source code or send this message (with error) to Issues at GitHub!\n");
+define("ERROR_JSON_DECODING", "Tried to decode parameters from JSON fomat, but failed!\n");
+define("ERROR_JSON_ENCODING", "Tried to encode parameters to JSON fomat, but failed!\n");
 
 /**
  * class JSONManager
@@ -42,7 +46,7 @@ class JSONManager
         $file_contents;
         $file_path = ROOT . $file;
 
-        if(!is_readable($file_path)) {
+        if (!is_readable($file_path)) {
             throw new Exceptions\FileNotFoundException($file_path);
         }
 
@@ -63,12 +67,11 @@ class JSONManager
 
     /**
      * Writes $contents to /$file (i.e. /configs/main.json), if file not present or not writeable throws FileNotFoundException,
-     * if present, but locked throws FileLockException, if writing failed throws FileWriteException, if succeeds returns true
+     *  if writing failed throws FileWriteException, if succeeds returns true
      *
      * @param string $file
      * @param string $contents
      * @throws Exceptions\FileNotFoundException
-     * @throws Exceptions\FileLockException
      * @throws Exceptions\FileWriteException
      * @return boolean
      */
@@ -77,18 +80,16 @@ class JSONManager
         $file_handler;
         $file_path = ROOT . $file;
 
-        if(!is_writeable($file_path)) {
+        if (!is_writeable($file_path)) {
             throw new Exceptions\FileNotFoundException($file_path);
         }
 
         $file_handler = fopen($file_path, "wb");
 
-        if (!flock($file_handler, LOCK_EX, $eWouldBlock) || $eWouldBlock) {
-            fclose($file_handler);
-            throw new Exceptions\FileLockException($file_path);
+        while (!flock($file_handler, LOCK_EX, $eWouldBlock)) {
         }
 
-        if(!fwrite($file_handler, $contents)) {
+        if (!fwrite($file_handler, $contents)) {
             fclose($file_handler);
             throw new Exceptions\FileWriteException($file_path);
         }
@@ -114,7 +115,12 @@ class JSONManager
         }
 
         $json = json_decode($file, true);
-        var_dump($json);
+
+        if (!json) {
+            $message = ERROR_PREFIX . ERROR_JSON_DECODING . ERROR_RECONFIG_REQUIRED;
+            $this->error_handler->print_error_and_redirect($this->logger, $message, "admin");
+            die;
+        }
 
         switch (strtolower($type)) {
             case "site":
@@ -126,6 +132,54 @@ class JSONManager
                 $message = ERROR_PREFIX . ERROR_INCORRECT_CONFIG_TYPE;
                 $this->error_handler->print_error_and_redirect($this->logger, $message, "admin");
                 die;
+        }
+    }
+
+    public function set_main_config(string $type, array $contents) : bool
+    {
+        try {
+            $file = $this->read_file("/configs/main.json");
+        } catch (\Exception $e) {
+            $message = ERROR_PREFIX . "(" . $e->getCode() . ")" . $e->getMessage() . "!\n" . ERROR_RECOFIG_REQUIRED;
+            $this->error_handler->print_error_and_redirect($this->logger, $message, "admin");
+            die;
+        }
+
+        $json_orig = json_decode($file, true);
+        
+        if (!json_orig) {
+            $message = ERROR_PREFIX . ERROR_JSON_DECODING;
+            $this->error_handler->print_error($this->logger, $message);
+            die;
+        }
+        
+        
+        foreach ($json_orig[$type] as $key => $value) {
+            foreach ($contents as $key1 => $value1) {
+                if ($key != $key1) {
+                    $message = ERROR_PREFIX . ERROR_CONFIG_STRUCTURE;
+                    $this->error_handler->print_error($this->logger, $message);
+                    die;
+                }
+                 
+                $json_orig[$type][$key] = $value1;
+            }
+        }
+
+        $json = json_encode($json_orig);
+
+        if (!$json) {
+            $message = ERROR_PREFIX . ERROR_JSON_ENCODING;
+            $this->error_handler->print_error($this->logger, $message);
+            die;
+        }
+
+        try {
+            $this->write_file($file, $json);
+        } catch (\Exception $e) {
+            $message = ERROR_PREFIX . "(" . $e->getCode() . ")" . $e->getMessage() . "!\n" . ERROR_CONFIG_UPDATE;
+            $this->error_hander->print_error($this->logger, $message);
+            die;
         }
     }
     
@@ -147,7 +201,12 @@ class JSONManager
         }
 
         $json = json_decode($file, true);
-        var_dump($json);
+        
+        if (!json) {
+            $message = ERROR_PREFIX . ERROR_JSON_DECODING . ERROR_RECONFIG_REQUIRED;
+            $this->error_handler->print_error_and_redirect($this->logger, $message, "admin");
+            die;
+        }
 
         switch (strtolower($type)) {
             case "users":
@@ -161,8 +220,9 @@ class JSONManager
             case "holywar":
                 return $json['comments'];
             default:
-                echo "Error!";
-                exit;
+                $message = ERROR_PREFIX . ERROR_QUERY_SELECT . ERROR_RECONFIG_REQUIRED;
+                $this->error_handler->print_error_and_redirect($this->logger, $message, "admin");
+                die;
         }
     }
 
@@ -187,5 +247,30 @@ class JSONManager
         }
     
         $json = json_decode($file, true);
+
+        if (!json) {
+            $message = ERROR_PREFIX . ERROR_JSON_DECODING . ERROR_RECONFIG_REQUIRED;
+            $this->error_handler->print_error_and_redirect($this->logger, $message, "admin");
+            die;
+        }
+
+        $json = $json[$lang];
+        var_dump($json);
+
+        switch(strtolower($section)) {
+            case 'errors':
+                return $json['errors'];
+            case 'info':
+                return $json['info'];
+            case 'user':
+                return $json['user'];
+            case 'panel':
+                return $json['panel'];
+            default:
+                $message = ERROR_PREFIX . ERROR_LOCALE_NOT_FOUND . ERROR_RECONFIG_REQUIRED;
+                $this->error_handler->print_error_and_redirect($this->logger, $message, "admin");
+                die;
+        }
+
     }
 }
