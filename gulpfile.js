35fdb7aead3c3ad93402c1a367480aeb2685b999
Vladimir <vovamaster99@gmail.com>
2018-12-25 09:08:29 +0300

    UI template development #2
    Working and ready-to-implement web-template

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 //Sass
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var source_dir = "proj/templates/RCSE/pages/**/";
-var final_dir = "proj/templates/RCSE/pages/";
+var source_dir = "public_html/themes/RCS-New/pages/**/";
+var final_dir = "public_html/themes/RCS-New/pages/";
 
 gulp.task('sass', function(){
-    gulp.src(source_dir + 'main.scss')
+    gulp.src(source_dir + 'structure.scss')
     .pipe(sass())
     .pipe(gulp.dest(final_dir))
 });
