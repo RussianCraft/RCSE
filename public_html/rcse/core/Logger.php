bdeac1be649ae567535320c65dfa05513f9972dc
Vladimir <vovamaster99@gmail.com>
2019-03-20 08:23:26 +0300

    Core development #2.4a

--- public_html/rcse/core/Logger.php
+++ public_html/rcse/core/Logger.php
@@ -1,24 +1,28 @@
 <?php
 declare(strict_types=1);
+
+use \Psr\Log\AbstractLogger;
+use \Psr\Log\LogLevel;
+
 namespace RCSE\Core;
 
-class Logger
+class Logger extends AbstractLogger
 {
     private $log_file;
     private $file_handler;
     private $message_levels = [
-        "Emergency",
-        "Alert",
-        "Critical",
-        "Error",
-        "Warning",
-        "Notice",
-        "Info",
-        "Debug"
+        LogLevel::EMERGENCY => 0,
+        LogLevel::ALERT => 1,
+        LogLevel::CRITICAL => 2,
+        LogLevel::ERROR => 3,
+        LogLevel::WARNING => 4,
+        LogLevel::NOTICE => 5,
+        LogLevel::INFO => 6,
+        LogLevel::DEBUG => 7
     ];
-    private $level_threshold;
+    private $level_threshold = LogLevel::DEBUG;
 
-    public function __construct(string $directory, int $level_threshold)
+    public function __construct(string $directory, int $level_threshold = LogLevel::DEBUG)
     {
 
     }
@@ -30,15 +34,23 @@ class Logger
     {}
 
     public function logSetLevelThreshold(int $level_threshold)
-    {}
+    {
+        $this->level_threshold = $level_threshold;
+    }
 
     public function logGetFilePath()
     {}
 
     private function logGetTimestamp()
-    {}
+    {
+        $millisBase = microtime(true);
+        $millis = spritf("%06d", ($millisBase - floor($millis)) * 1000000);
+        $date = new DateTime(date('Y-m-d H:i:s.'.$millis, $millisBase));
 
-    public function logWrite(int $level, string $message)
+        return $date->format('Y-m-d H:i:s.v');
+    }
+
+    public function log(int $level, string $message, array $context = array())
     {}
 
     private function logFormatMessage(int $level, string $message)
