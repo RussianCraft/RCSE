96f07c8bb3863384bb6451be454268ecff078722
Vladimir <siluet-stalker99@yandex.ru>
2019-04-24 00:34:59 +0300

    Main theme development #0.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 //Sass
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var sourceDir = "public_html/themes/RCS-New/pages/**/";
-var finalDir = "public_html/themes/RCS-New/pages/";
+var sourceDir = "public_html/resources/themes/RCS/pages/**/";
+var finalDir = "public_html/resources/themes/RCS/pages/";
 
 gulp.task("sass", function(){
     gulp.src(sourceDir + "structure.scss")
