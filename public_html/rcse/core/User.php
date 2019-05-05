3813f520f1cbc50d6b2e2e65cb5104d626026bca
Vladimir <siluet-stalker99@yandex.ru>
2019-05-06 00:54:46 +0300

    Core development #2.7a

--- public_html/rcse/core/User.php
+++ public_html/rcse/core/User.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 namespace RCSE\Core;
 
 class User
@@ -13,27 +13,35 @@ class User
     /** @var Logger */
     private $logger;
 
+    /** @var Utils */
+    private $utils;
+
+    /** @var File */
+    private $file;
+
     public function __construct(Logger $logger, Configurator $config)
     {
         $this->logger = $logger;
         $this->config = $config;
+        $this->utils = new Utils();
+        $this->file = new File();
         $this->database = new Database($this->logger, $this->config);
     }
 
     public function userRegister(string $login, string $password, string $email, string $sex, string $birthdate, string $origin)
     {
-        if($this->database->databaseCheckData('accounts','by_login',$login)) {
+        if ($this->database->databaseCheckData('accounts', 'by_login', $login)) {
             $message = "Failed to register account: login {$login} is already claimed!";
             $this->logger->log($this->logger::ERROR, $message, get_class($this));
-            throw new \Exception($message, 1020);
+            return $message;
         }
-        if($this->database->databaseCheckData('accounts','by_email',$email)) {
+        if ($this->database->databaseCheckData('accounts', 'by_email', $email)) {
             $message = "Failed to register account: email {$email} is already claimed!";
             $this->logger->log($this->logger::ERROR, $message, get_class($this));
-            throw new \Exception($message, 1020);
+            return $message;
         }
 
-        $birthdate = new DateTime($birthdate).format('Y-m-d');
+        $birthdate = new DateTime($birthdate) . format('Y-m-d');
         $regdate = date('Y-m-d');
         $passhash = password_hash($password, PASSWORD_ARGON2I);
         $settings = [
@@ -51,32 +59,94 @@ class User
             ":regdate" => $regdate,
             ":settings" => json_encode($settings)
         ];
-        
+
+        $file_path = "/userdata/{$login}/";
+        $file_name = "session.json";
+
+        file_put_contents($file_path.$file_name,"{}");
+
         try {
             return $this->database->databaseSendData('accounts', 'insert', $params);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
+            $this->logger->log($this->logger::ERROR, $e->getMessage(), get_class($this));
+            return false;
+        }
+    }
+
+    public function userLogin(string $id, string $pass)
+    {
+        if (strpos($id, '@') !== false && strpos($id, '.') !== false) {
+            $type = "by_email";
+        } else {
+            $type = "by_login";
+        }
+
+        if ($this->database->databaseCheckData('accounts', $type, $id) === false) {
+            $message = "Failed to login: account for {$id} not found!";
+            $this->logger->log($this->logger::ERROR, $message, get_class($this));
+            return $message;
+        }
+
+        try {
+            $user_data = $this->database->databaseGetData('accounts', $type, $id);
+        } catch (\Exception $e) {
             $this->logger->log($this->logger::ERROR, $e->getMessage(), get_class($this));
             return false;
         }
 
+        if (password_verify($pass, $user_data['password']) === false) {
+            $message = "Failed to login: password doesn't match!";
+            $this->logger->log($this->logger::NOTICE, $message, get_class($this));
+            return $message;
+        }
     }
 
-    public function userLogin()
-    {}
-
     public function userVerify()
-    {}
+    { }
 
     public function userPunish()
-    {}
+    { }
 
     public function userEdit()
-    {}
+    { }
 
     public function userSuspend()
-    {}
+    { }
 
     public function userRemove()
+    { }
+
+    private function userSessionCreate(string $login)
     {
+        $file_path = "/userdata/{$login}/";
+        $file_name = "session.json";
+
+        $session_id = Utils::utilsRandomString(16);
+        
+        try {
+            $session_file_data = $this->file->fileRead($file_path, $file_name);
+        } catch(\Exception $e) {
+            $this->logger->log($this->logger::ERROR, $e->getMessage(), get_class($this));
+            return false;
+        }
+    }
+
+    private function userSessionObtain(string $login, string $session_id)
+    { }
+
+    private function userSessionValidate(string $login, string $session_id)
+    { }
+
+    private function userGetIP(): string
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ip;
     }
 }
---
+++ public_html/rcse/core/Utils.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+namespace RCSE\Core;
+
+class Utils 
+{
+    public static function utilsRandomString($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    {
+        $pieces = [];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $pieces []= $keyspace[random_int(0, $max)];
+        }
+        return implode('', $pieces);
+    }
+}
